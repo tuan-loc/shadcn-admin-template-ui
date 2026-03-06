6ff2bc39d1ffd94fc3f676a939893633399c6482
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-06 09:53:11 +0700

    implement react-router-dom for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import Router from './router'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/*' element={<Router />} />)
+)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
