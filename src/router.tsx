1a684519b8d68305b20e8d9c333997577d3200f2
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-08 21:46:47 +0700

    add error pages

--- src/router.tsx
+++ src/router.tsx
@@ -5,18 +5,30 @@ import Loader from '@/components/loader'
 const Dashboard = lazy(() => import('@/pages/dashboard'))
 const SignIn = lazy(() => import('@/pages/auth/sign-in'))
 const SignIn2 = lazy(() => import('@/pages/auth/sign-in-2'))
-const ForgotPassword = lazy(() => import('@/pages/auth/forgot-password'))
 const SignUp = lazy(() => import('@/pages/auth/sign-up'))
+const ForgotPassword = lazy(() => import('@/pages/auth/forgot-password'))
+
+// Errors
+const GeneralError = lazy(() => import('@/pages/errors/general-error'))
+const NotFoundError = lazy(() => import('@/pages/errors/not-found-error'))
+const MaintenanceError = lazy(() => import('@/pages/errors/maintenance-error'))
 
 export default function Router() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        {/* Auth routes */}
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-in-2' element={<SignIn2 />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
+
+        <Route path='/' element={<Dashboard />} />
+
+        {/* Error routes */}
+        <Route path='/500' element={<GeneralError />} />
+        <Route path='/404' element={<NotFoundError />} />
+        <Route path='/503' element={<MaintenanceError />} />
       </Routes>
     </Suspense>
   )
